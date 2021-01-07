77987190b8b324feb0255abbea579a77b8fa7ed0
Julia Litvinoff Justus <julialiff@hotmail.com>
2021-01-07 17:27:44 -0300

    Melhoria design pedidos

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -6,7 +6,7 @@ module OrdersHelper
   def total(numero_pedido)
     total = 0
     Order.where(numero_pedido: numero_pedido).each do |o|
-      total += o.preco
+      total += (o.preco * o.quantidade)
     end
     total
   end

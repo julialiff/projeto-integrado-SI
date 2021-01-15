582ec4761521378b9c3aab80dd85d1e904d99282
victor taendy <taendy.s@usp.br>
2021-01-14 21:18:39 -0300

    testes categoria e comerciante

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Cria uma categoria" do
+
+	category_qtt = Category.all.length()
+  	category = Category.create nome: "Mascara tipo1"
+
+	category_qtt_new = Category.all.length()
+
+	category_qtt_new.equal? (1+category_qtt)
+    Category.where("nome = 'Mascara tipo1'").first.equal? category
+
+  end
+
+  it "Edita uma categoria" do
+    
+    category = Category.create nome: "Mascara tipo2"
+    
+    category_before = category
+
+	category.update(
+            nome: "Nova Mascara 3"
+        )
+
+	!(category.equal? category_before)
+    !(category.nome.equal? category_before.nome)
+	
+  end
 end
